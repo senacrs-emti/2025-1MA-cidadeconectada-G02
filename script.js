e4ac880cadb8cf9d8f8a40a4b8b948d7939cbcea
TulyowZ <arthurrteixeira2010@gmail.com>
2025-11-05 12:28:13 -0300

    footer terminado
    adicionem depois o nome do html pra linkar ou a fonte se tiverem

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("contatos").addEventListener("click", () => {window.location.href = "outra_pagina.html"; });
+document.getElementById("whatsapp").addEventListener("click", () => {window.location.href = "outra_pagina.html"; });
+document.getElementById("instagram").addEventListener("click", () => {window.location.href = "outra_pagina.html"; });
