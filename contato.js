776175a36f122a41fae1bbfc7d9f6e9a68ca027a
Maurício C. Deus <mauriciodeus42@gmail.com>
2025-11-17 11:34:53 -0300

    adicionei js
    mas falta corrigir o html e o css

---
+++ contato.js
@@ -0,0 +1,2 @@
+document.getElementById("whatsapp").addEventListener("click", () => {window.location.href = "outra_pagina.html"; });
+document.getElementById("instagram").addEventListener("click", () => {window.location.href = "outra_pagina.html"; });
