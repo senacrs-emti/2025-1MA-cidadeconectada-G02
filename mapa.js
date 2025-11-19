2509008641ab548d99ee05bf2b5f77d7aed627a8
Gabriel.SK <gabrielkonrad.rs@gmail.com>
2025-11-19 12:28:35 -0300

    Mapa criado
    Mapa feito, colocado dois marcadores e feito a coleta da localização do usuário para melhor utilização do mapa

---
+++ mapa.js
@@ -0,0 +1,23 @@
+
+function sucesso(position){
+	console.log(position.coords.latitude, position.coords.longitude);
+
+	var map = L.map('map').setView([position.coords.latitude, position.coords.longitude], 17);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([-30.026317061493387, -51.21209834262374]).addTo(map);
+
+var marker2 = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+
+
+marker.bindPopup("<b>Escola de Ensino Médio Senac Distrito Criativo</b> <br> <spam>Particular</spam>").openPopup();
+
+marker2.bindPopup("<b>Sua localização</b>").openPopup();
+
+}
+
+navigator.geolocation.getCurrentPosition(sucesso);
