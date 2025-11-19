1112fbb8dab841992bd5c29ac33dfdc729c784fb
Maurício C. Deus <mauriciodeus42@gmail.com>
2025-11-19 11:52:20 -0300

    Contato 100%
    terminei

--- contato.js
+++ contato.js
@@ -1,2 +1,20 @@
-document.getElementById("whatsapp").addEventListener("click", () => {window.location.href = "outra_pagina.html"; });
-document.getElementById("instagram").addEventListener("click", () => {window.location.href = "outra_pagina.html"; });
+document.getElementById("button").addEventListener("click", () => {
+    
+    const nome = document.getElementById("text");
+    const email = document.getElementById("email");
+    const mensagem = document.getElementById("message");
+
+    const vNome = nome.value.trim();
+    const vEmail = email.value.trim();
+    const vMensagem = mensagem.value.trim();
+
+    if (vNome === "" || vEmail === "" || vMensagem === "") {
+        alert("Não há mensagem");
+    } else {
+        alert("Enviado");
+
+        nome.value = "";
+        email.value = "";
+        mensagem.value = "";
+    }
+});
