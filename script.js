a6a2129e4ed891430499d9845f1382acd1691338
Lauren A.C. <mingufofo@gmail.com>
2025-11-24 11:06:22 -0300

    pasta para imagens, remoção de página  desnecessária, adição de script
    Pasta feita pra organizar as imagens, o "mapa escolas.html" era desnecessário e um novo script foi adicionado. Como as imagens foram movidas para uma nova pasta, o css foi mudado para acomodar essa mudança

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("botaoinicio").addEventListener("click", () => {
+    window.location.href = "index.html"; 
+});
+document.getElementById("botaomapa").addEventListener("click", () => {
+    window.location.href = "mapa.html"; 
+});
+document.getElementById("botaocontatos").addEventListener("click", () => {
+    window.location.href = "contatos.html"; 
+});
