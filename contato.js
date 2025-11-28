2f6e4dac0f08420f985d19d7733ec36e4f847237
Maurício C. Deus <mauriciodeus42@gmail.com>
2025-11-28 12:04:25 -0300

    vou terminar em casa

--- contato.js
+++ contato.js
@@ -18,3 +18,23 @@ document.getElementById("botao").addEventListener("click", () => {
         mensagem.value = "";
     }
 });
+
+document.getElementById("whatsapp").addEventListener("click", () => {
+    window.location.href = "https://wa.me/51999995555"; 
+});
+
+document.getElementById("instagram").addEventListener("click", () => {
+    window.location.href = "https://www.instagram.com"; 
+});
+
+document.getElementById("botaoinicio").addEventListener("click", () => {
+    window.location.href = "index.html"; 
+});
+
+document.getElementById("botaomapa").addEventListener("click", () => {
+    window.location.href = "mapa.html"; 
+});
+
+document.getElementById("botaocontatos").addEventListener("click", () => {
+    window.location.href = "contatos.html"; 
+});
